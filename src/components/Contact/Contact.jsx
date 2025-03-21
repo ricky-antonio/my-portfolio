48fbf3634adf9f96bec2bca44fc785e15accb267
Ricardo Monterrsoa <48804047+ricky-antonio@users.noreply.github.com>
2025-03-21 13:03:04 -0700

    Add Contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,40 @@
+import { BsFillSendFill } from "react-icons/bs";
+import Pyramid from "../../ui/Pyramid";
 import "./Contact.css";
 
 const Contact = () => {
-  return (
-    <div>
-      <h2>contact</h2>
-    </div>
-  )
-}
+    return (
+        <section id="contact">
+            <div className="section__wrapper">
+                <div className="pyramid__container"><Pyramid /></div>
 
-export default Contact
+                <div className="contact__group">
+                    <div>
+                        <h3 className="title">
+                            Let’s Build{" "}
+                            <span className="shine">Something Amazing</span>{" "}
+                            Together!
+                        </h3>
+                    </div>
+                <form action="">
+                    <input type="text" placeholder="Name" name="name" required />
+                    <input type="email" placeholder="Email" name="email" required />
+                    <textarea
+                        name="message"
+                        rows={7}
+                        placeholder="Your message"
+                    ></textarea>
+                    <button type="button" className="btn flex__center submit__btn">
+                        <div className="icon">
+                            <BsFillSendFill />
+                        </div>
+                        <span>Send Now</span>
+                    </button>
+                </form>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Contact;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,21 +1,22 @@
-import { projects } from "../../data"
-import ProjectCard from "../../ui/ProjectCard/ProjectCard"
-import "./Projects.css"
+import { projects } from "../../data";
+import ProjectCard from "../../ui/ProjectCard/ProjectCard";
+import "./Projects.css";
 
 const Projects = () => {
-  return (
-    <section id="projects">
-      <div className="section__wrapper"></div>
-      <div className="section__header">
-        <h2 className="shine">Projects</h2>
-      </div>
-      <div className="projects__container">
-        {projects.map((project, index) => (
-            <ProjectCard {...project} key={index} />
-        ))}
-      </div>
-    </section>
-  )
-}
+    return (
+        <section id="projects">
+            <div className="section__wrapper">
+                <div className="section__header">
+                    <h2 className="shine">Projects</h2>
+                </div>
+                <div className="projects__container">
+                    {projects.map((project, index) => (
+                        <ProjectCard {...project} key={index} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+};
 
-export default Projects
+export default Projects;
