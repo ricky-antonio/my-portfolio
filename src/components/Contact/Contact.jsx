a2715469f52793eaf4f8131ae904d5ff524a0eea
Ricardo Monterrsoa <48804047+ricky-antonio@users.noreply.github.com>
2025-03-21 15:30:01 -0700

    Connect contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,36 +1,118 @@
 import { BsFillSendFill } from "react-icons/bs";
 import Pyramid from "../../ui/Pyramid";
 import "./Contact.css";
+import { useState } from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+const EMAILJS_SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+const EMAILJS_TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+const EMAILJS_USER_ID = import.meta.env.VITE_EMAILJS_USER_ID;
 
 const Contact = () => {
+    const [emailSent, setEmailSent] = useState(false);
+
+    const formRef = useRef();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setEmailSent(true)
+
+        // Check honeypot field
+        const formData = new FormData(formRef.current);
+        const honeypotValue = formData.get("honeypot");
+        if (honeypotValue) {
+            console.log("Spam detected. Submission rejected.");
+            return; // Exit and do not send the email
+        }
+
+        // send email
+        emailjs
+            .sendForm(
+                EMAILJS_SERVICE_ID, // Service ID
+                EMAILJS_TEMPLATE_ID, // Template ID
+                formRef.current,
+                EMAILJS_USER_ID // User ID
+            )
+            .then((result) => {
+                console.log("Email sent successfully!", result.text);
+            })
+            .catch((error) => {
+                console.error("Error sending email:", error.text);
+            });
+    };
+
     return (
         <section id="contact">
             <div className="section__wrapper">
-                <div className="pyramid__container"><Pyramid /></div>
+                <div className="pyramid__container">
+                    <Pyramid />
+                </div>
 
                 <div className="contact__group">
-                    <div>
+                    <div className="cta">
                         <h3 className="title">
                             Let’s Build{" "}
                             <span className="shine">Something Amazing</span>{" "}
                             Together!
                         </h3>
                     </div>
-                <form action="">
-                    <input type="text" placeholder="Name" name="name" required />
-                    <input type="email" placeholder="Email" name="email" required />
-                    <textarea
-                        name="message"
-                        rows={7}
-                        placeholder="Your message"
-                    ></textarea>
-                    <button type="button" className="btn flex__center submit__btn">
-                        <div className="icon">
-                            <BsFillSendFill />
-                        </div>
-                        <span>Send Now</span>
-                    </button>
-                </form>
+                    <div className="form__container">
+                        {!emailSent && (
+                            <form
+                                action="submit"
+                                ref={formRef}
+                                onSubmit={handleSubmit}
+                            >
+                                <input
+                                    type="text"
+                                    placeholder="Name"
+                                    name="name"
+                                    required
+                                />
+                                <input
+                                    type="email"
+                                    placeholder="Email"
+                                    name="email"
+                                    required
+                                />
+                                <textarea
+                                    name="message"
+                                    rows={7}
+                                    placeholder="Your message"
+                                    required
+                                ></textarea>
+                                {/* Honeypot field */}
+                                <input
+                                    type="text"
+                                    name="honeypot"
+                                    style={{ display: "none" }}
+                                    autoComplete="off"
+                                />
+                                <button
+                                    type="submit"
+                                    className="btn flex__center submit__btn"
+                                >
+                                    <div className="icon">
+                                        <BsFillSendFill />
+                                    </div>
+                                    <span>Send Now</span>
+                                </button>
+                            </form>
+                        )}
+                        {emailSent && (
+                            <h3
+                                className={`title ${
+                                    emailSent && "email__sent"
+                                }`}
+                            >
+                                Thank you for reaching out,
+                                <br />
+                                I'll be in touch soon!
+                            </h3>
+                        )}
+                    </div>
                 </div>
             </div>
         </section>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,36 @@
-import "./Footer"
+import { Link } from "react-scroll";
+import { menu } from "../../data";
+import "./Footer.css";
+import SocialHandles from "../../ui/SocialHandles/SocialHandles";
 
 const Footer = () => {
-  return (
-    <div>
-      <h2>footer</h2>
-    </div>
-  )
-}
+    return (
+        <footer>
+            <div className="section__wrapper">
+                <ul className="flex__center nav">
+                    {menu.map((item, index) => (
+                        <Link
+                            key={index}
+                            to={item.name.toLowerCase()}
+                            smooth={true}
+                            duration={500}
+                            className="nav__item"
+                        >
+                            {item.name}
+                        </Link>
+                    ))}
+                </ul>
+                <SocialHandles />
+                <div className="copyright">
+                    <h3>&copy; 2025 Ricardo Monterrosa. All rights reserved.</h3>
+                </div>
+                <div className="base__logo">
+                    <h1 className="full__name shine">Ricardo Monterrosa</h1>
+                </div>
+    
+            </div>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
